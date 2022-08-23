1d6b8e6d31d488ac49610a5a4f250d0f8c1a2bc5
lenovo <srishtijee1@gmail.com>
2022-08-23 09:27:06 -0700

    third commit

---
+++ tut5.c
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+
+ 
+ int main()
+{
+// float c= (float)(a)/b;
+// printf("the result is %f",c);
+//implicit 
+int a=67;
+char ch ='a';
+float sum =  (float)(a)/'a';
+printf("the rwsult is %f",sum);t
+
+
+
+    return 0;
+}
